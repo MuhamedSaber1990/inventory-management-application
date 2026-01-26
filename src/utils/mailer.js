464dc4c2d33295f55654f3cb28833dc1c70b2279
SWARDHUNTER\User <mkenawi381@gmail.com>
2026-01-26 04:26:56 -0800

    fixing mailer Compatibility with  render test 2

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -11,6 +11,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
 });
 
 export async function sendResetPWEmail(toEmail, resetLink) {
